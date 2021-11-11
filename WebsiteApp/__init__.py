48d190545cd02d2c7c4b377d80f904bc4f659a7f
h0se_69 <joseplaysmc99@gmail.com>
2021-11-10 18:46:45 -0800

    Initialize vars
    > Database
    > Flask OBJ

---
+++ WebsiteApp/__init__.py
@@ -0,0 +1,17 @@
+import os
+import flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app_Obj = flask.Flask(__name__) 
+app_Obj.config.from_mapping (
+    SECRET_KEY = 'it-dont-matter',
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+)
+
+db = SQLAlchemy(app_Obj)
+
+from WebsiteApp import routes, models
